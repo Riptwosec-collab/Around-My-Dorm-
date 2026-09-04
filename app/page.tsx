8b19b1375537dfbf0518957cf8fa19457e4289db
Riptwosec-collab <aidsaras@gmail.com>
2026-09-04 07:50:14 +0700

    Restore original app home UI while keeping expanded data

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,5 @@
 import { AroundMyDormApp } from "@/components/AroundMyDormApp";
-import { NewPlacesSpotlight } from "@/components/NewPlacesSpotlight";
 
 export default function HomePage() {
-  return (
-    <>
-      <NewPlacesSpotlight />
-      <AroundMyDormApp initialTab="explore" />
-    </>
-  );
+  return <AroundMyDormApp initialTab="explore" />;
 }
